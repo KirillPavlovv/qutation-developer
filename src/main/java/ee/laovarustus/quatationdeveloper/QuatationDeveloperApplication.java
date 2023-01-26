6db528b614e0388199fb27b62c353b75bcde41cc
Kirill Pavlov <kirillp1988@gmail.com>
2023-01-26 19:43:20 +0200

    initial commit

---
+++ src/main/java/ee/laovarustus/quatationdeveloper/QuatationDeveloperApplication.java
@@ -0,0 +1,13 @@
+package ee.laovarustus.quatationdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuatationDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuatationDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/laovarustus/quatationdeveloper/QuatationDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.laovarustus.quatationdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuatationDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
