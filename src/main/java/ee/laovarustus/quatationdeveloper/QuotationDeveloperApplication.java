15394fda402101e233d66957c205f1db85a79a5e
Kirill Pavlov <kirillp1988@gmail.com>
2023-01-26 22:45:01 +0200

    correct typo

--- src/main/java/ee/laovarustus/quatationdeveloper/QuotationDeveloperApplication.java
+++ src/main/java/ee/laovarustus/quatationdeveloper/QuotationDeveloperApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuatationDeveloperApplication {
+public class QuotationDeveloperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuatationDeveloperApplication.class, args);
+		SpringApplication.run(QuotationDeveloperApplication.class, args);
 	}
 
 }
