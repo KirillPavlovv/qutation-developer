e210e82ac04c271ae3e11b817d4b804ef16cc608
Kirill Pavlov <kirillp1988@gmail.com>
2023-02-09 22:42:15 +0200

    add card for product-group

--- ui/src/app/new-quatation/product-list/product-group/product-group.component.ts
+++ ui/src/app/new-quatation/product-list/product-group/product-group.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Product} from "./product-item/product.model";
 
 @Component({
   selector: 'app-product-group',
@@ -8,18 +6,8 @@ import {Product} from "./product-item/product.model";
   styleUrls: ['./product-group.component.css']
 })
 export class ProductGroupComponent implements OnInit {
-
-  products: Product[];
-  productGroups: Product[];
-
-  constructor(private http: HttpClient) {
-  }
-
   ngOnInit(): void {
-    this.http.get<Product[]>('http://localhost:8080/product')
-      .subscribe(products => {
-        this.products = products;
-        console.log(this.products)
-      });
   }
+
+
 }
--- ui/src/app/new-quatation/product-list/product-list.component.ts
+++ ui/src/app/new-quatation/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Product} from "./product-group/product-item/product.model";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  products: Product[];
+  productGroups: Product[];
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Product[]>('http://localhost:8080/product')
+      .subscribe(products => {
+        this.products = products;
+        console.log(this.products)
+      });
+  }
 }
